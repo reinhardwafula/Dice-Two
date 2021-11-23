10a150b531d17a107cbd873f164a56cfc5ff84f5
reinhardwafula <rehynsanders@gmail.com>
2021-11-23 17:21:25 +0300

    Dice Images Set

---
+++ app/src/main/java/com/example/dicerollertwo/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.dicerollertwo
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice(){
+        val randomInt = (1..6).random()
+        var diceImage: ImageView = findViewById(R.id.dice_image)
+        var drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
